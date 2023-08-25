e7b4841725abdcbbfaae8ce23af46a5cb0e6e29a
Dionicio <dionicio.pl06@gmail.com>
2023-08-25 12:08:07 -0600

    agregando documentacion

--- src/data_load.py
+++ src/data_load.py
@@ -4,7 +4,24 @@ from keras.utils import Sequence
 from keras.preprocessing.image import ImageDataGenerator
 
 class CustomImageDataGenerator(Sequence):
+    """Creamos una clase la cuales es un generador de imagenes con las caracteristicas especiales para este proyecto
+    """
     def __init__(self, image_paths_noisy, image_paths_clean , batch_size, target_size, class_mode='input',shuffle=True, datagen_args=None):
+        """Funcion iniciadora de la clase, incluye los valores con los que inicializa
+        
+        En esta funcion cargamos las imagenes con imperfecciones y en buene estado, las procesamos bajo el mismo ImageDataGenerador
+        y regreamos las imagenes de tal forma que "x" es la imagen con imperfeccion procesada con el ImageDataGenerator y
+        "y"  es la imagen en buen estado procesada bajo el mismo ImageDataGenerator
+
+        Args:
+            image_paths_noisy (list): lista con las rutas de las imagenes con imperfecciones
+            image_paths_clean (list): lista con las rutas de las imagenes en buen estado
+            batch_size (int): batch_sie
+            target_size ((int, int)): tuple con el tamaño de las imagnes
+            class_mode (str, optional): Defaults to 'input'.
+            shuffle (bool, optional): Activar o no modo aleatorio.
+            datagen_args (_type_, optional): Funcion ImageDataGenerador, se describe como sera
+        """
         self.image_paths_noisy = image_paths_noisy
         self.image_paths_clean = image_paths_clean
         self.batch_size = batch_size
